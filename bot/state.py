c9b0af93d564436972807ceed1e0f1ffc53b1267
kondrashkinas727-dotcom <kondrashkina.s727@email.com>
2025-12-25 01:13:49 +0300

    Final deploy: FSM, Telethon, parsing, SQLite

--- bot/state.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-user_languages = {}
--- bot/states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class AuthState(StatesGroup):
-    phone = State()
-    code = State()
-    password = State()
-
-class ParseState(StatesGroup):
-    chat = State()
